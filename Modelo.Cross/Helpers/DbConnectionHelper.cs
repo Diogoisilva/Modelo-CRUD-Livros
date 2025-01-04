52de4168f11c3ba4e652f9738c3e4f789a6d4df2
DiogoIsilva <diogoignaciosilva@gmail.com>
2025-01-04 19:15:37 -0300

    incluido projeto cross - será usado para compatilhar, centralizar e reutilizar os códigos se necessario;

---
+++ Modelo.Cross/Helpers/DbConnectionHelper.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+
+
+namespace Modelo.Cross.Helpers
+{
+    public static class DbConnectionHelper
+    {
+        private static string _connectionString = "Mudar isso depois quando começar a conectar, puxar a connection string do appsetting";
+
+        public static SqlConnection GetConnection()
+        {
+            return new SqlConnection(_connectionString);
+        }
+
+        public static void OpenConnection(SqlConnection connection)
+        {
+            if (connection.State != ConnectionState.Open)
+                connection.Open();
+        }
+
+        public static void CloseConnection(SqlConnection connection)
+        {
+            if (connection.State != ConnectionState.Closed)
+                connection.Close();
+        }
+    }
+}
