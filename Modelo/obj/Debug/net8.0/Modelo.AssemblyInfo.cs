ab57aa24d908c3059d63aa7457e1a74abc23789a
Diogoisilva <96752886+Diogoisilva@users.noreply.github.com>
2025-01-05 13:24:58 -0300

    Revert "Feat inclusao projeto services"

--- Modelo/obj/Debug/net8.0/Modelo.AssemblyInfo.cs
+++ Modelo/obj/Debug/net8.0/Modelo.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Modelo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2d8b8de54877328ddf09601a349d05d1030fe646")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Modelo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Modelo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
